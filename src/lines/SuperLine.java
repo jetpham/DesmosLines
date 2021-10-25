4b7a3023368c030bb02cd11ace2acff085f96b77
Jet Pham <jetthomaspham@gmail.com>
2021-10-24 17:04:29 -0700

    Fixed Capitalization in the SuperLine class description

--- src/lines/SuperLine.java
+++ src/lines/SuperLine.java
@@ -1,7 +1,7 @@
 package src.lines;
 
 /**
- * superline is an abstract class that represents any line in the lines package
+ * SuperLine is an abstract class that represents any line in the lines package
  */
 public abstract class SuperLine {
     public abstract String lineForDesmos();
