c69cd9a6880ddba24a82f488b1e770717c45eafd
Jet Pham <jetthomaspham@gmail.com>
2021-10-24 17:01:11 -0700

    Added a description of the functionality of the SuperLine Abstract class

--- src/lines/SuperLine.java
+++ src/lines/SuperLine.java
@@ -1,5 +1,8 @@
 package src.lines;
 
+/**
+ * superline is an abstract class that represents any line in the lines package
+ */
 public abstract class SuperLine {
     public abstract String lineForDesmos();
 
